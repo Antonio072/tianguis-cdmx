2e34af95f32f9a34df762d6342bace0805567764
kikemaya <enrique.mg.cd@gmail.com>
2022-10-08 14:51:49 -0500

    boton generico naranja :white_check_mark:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-    content: [
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/modules/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      ...colors
+    }
+  },
+  plugins: []
+};
