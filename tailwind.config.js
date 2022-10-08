086b50ec6a8752938143e09dd965754b330c0f00
kikemaya <enrique.mg.cd@gmail.com>
2022-10-08 17:24:21 -0500

    :sparkles: boton generico naranja volvio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./src/modules/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {}
+    extend: {
+      ...colors
+    }
   },
   plugins: []
 };
