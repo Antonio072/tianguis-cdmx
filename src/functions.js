30149ffc5d89f868a88d04b42a51562d580cb4ee
José Antonio Velázquez Mandujano <jantonio.vema@gmail.com>
2022-12-04 09:35:39 -0600

    feat: Add como llegar link

--- src/functions.js
+++ src/functions.js
@@ -8,7 +8,8 @@ const clearMarkers = (markers = Set(), map) => {
 const description = (point) => {
     return `<div><b>Ubicación:</b> ${point['Ubic']}</div>
             <div><b>Horario:</b> ${point['Horario']}</div>
-            <div><b>Colonia:</b> ${point['Nombre Col']}</div>`;
+            <div><b>Colonia:</b> ${point['Nombre Col']}</div>
+            <div><a target="_blank"href="https://maps.google.com/?q=${point['Latitude']},${point['Longitude']}">¿Cómo llegar?</a><div>`;
 };
 
 export { clearMarkers, description };
