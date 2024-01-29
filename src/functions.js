0bbaeae6ef10e6684643af59ba5ea767c6c3d56b
José Antonio Velázquez Mandujano <jantonio.vema@gmail.com>
2024-01-28 21:50:44 -0600

    Add colored days

--- src/functions.js
+++ src/functions.js
@@ -79,4 +79,23 @@ async function setLocationOnMap(map){
   return location
 }
 
-export { clearMarkers, description, daysInitialLetters, setLocationOnMap };
+
+function paintIconByDay(dayColor){
+  const ICON = `<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+    <!-- Uploaded to: SVG Repo, www.svgrepo.com, Transformed by: SVG Repo Mixer Tools -->
+    <svg fill="${dayColor}" width="30px" height="30px" viewBox="-3.2 -3.2 38.40 38.40" xmlns="http://www.w3.org/2000/svg">
+    <g id="SVGRepo_bgCarrier" stroke-width="0">
+      <rect x="-3.2" y="-3.2" width="38.40" height="38.40" rx="19.2" fill="#000000" strokewidth="0.5"/>
+    </g>
+    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
+    <g id="SVGRepo_iconCarrier"> <circle cx="16" cy="16" r="16"/> </g>
+    </svg>`;
+  console.log(dayColor)
+  let iconUrl = 'data:image/svg+xml;base64,' + btoa(ICON);
+
+    return new L.icon({
+      iconUrl: iconUrl,
+    });
+}
+
+export { clearMarkers, description, daysInitialLetters, setLocationOnMap, paintIconByDay };
